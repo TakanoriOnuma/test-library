31e35d8af36da41132544908d0665716b0c80980
Onuma <ohnhistory@gmail.com>
2018-12-22 22:38:09 +0900

    ライブラリ内でlodashを使う

--- lib.js
+++ lib.js
@@ -1,9 +1,15 @@
 const path = require('path');
+const { times, fill } = require('lodash');
 
 const testJoin = (a, b) => {
   return path.join(a, b);
 };
 
+const fillArray = (arrayNum, value) => {
+  return times(arrayNum).fill(value);
+};
+
 module.exports = {
-  testJoin
+  testJoin,
+  fillArray
 };
